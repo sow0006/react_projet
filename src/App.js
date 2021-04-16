ddf5e29ccc7fe7178403f5edbe0c348a1d22aefc
sow0006 <ibraasow@gmail.com>
2021-04-16 17:39:53 +0200

    tableaux

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import React, { Component } from 'react';
 import Card from './Card'
 import GuessCount from './GuessCount'
+import './App.css'
 
+import shuffle from 'lodash.shuffle'
+import HallOfFame, { FAKE_HOF } from './HallOfFame';
+
+const SIDE = 6
+const SYMBOLS = '😀🎉💖🎩🐶🐱🦄🐬🌍🌛🌞💫🍎🍌🍓🍐🍟🍿'
 
 class App extends Component{
+  cards = this.generateCards()
 
+  generateCards() {
+    const result = []
+    const size = SIDE * SIDE
+    const candidates = shuffle(SYMBOLS)
+    while (result.length < size){
+      const card = candidates.pop()
+      result.push(card,card)
+    }
+    return shuffle(result)
+  }
   handleCardClick (card) {
     console.log(card,'clicked')
   }
   render() {
     const won = new Date().getSeconds() % 2 === 0
-    return <div classNamne="memory"> 
+    return <div className="memory"> 
       <GuessCount guesses={0}/>
-      <Card card="😀" feedback="hidden" onClick={this.handleCardClick}/>
-      <Card card="🎉" feedback="justMatched" onClick={this.handleCardClick}/>
-      <Card card="💖" feedback="justMismatched" onClick={this.handleCardClick}/>
-      <Card card="🎩" feedback="visible" onClick={this.handleCardClick}/>
-      <Card card="🐶" feedback="hidden" onClick={this.handleCardClick}/>
-      <Card card="🐱" feedback="justMatched" onClick={this.handleCardClick}/>
-      {won && <p>GAGNÉ !</p>}
+      {this.cards.map((card,index) => (
+        <Card 
+          card= {card} 
+          feedback="visible" 
+          key={index} 
+          onClick={this.handleCardClick}/>
+      ))}
+      {won && <HallOfFame entries={FAKE_HOF} />}
     </div>
   }
 }
