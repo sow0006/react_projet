d959d154d89f14ad6ca953b2cf060b91a8902511
sow0006 <ibraasow@gmail.com>
2021-04-16 15:48:51 +0200

    composant carte

---
+++ src/Card.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import './Card.css'
+
+const HIDDEN_SYMBOL = '❓'
+
+const Card = () => <div className="card" />
+
+const Card = ({card, feedback}) => (
+    <div className={'card ${feedback}'}>
+        <span className="symbol">
+            {feedback === 'hidden' ? HIDDEN_SYMBOL : card}
+        </span>
+    </div>
+)
+
+export default Card
